fb982a6ca34446480c3ba8bd65e9b3c923677a3b
Vladi <vladislav.tyutin@me.com>
2021-11-26 16:10:50 +0100

    fix accuracy function

--- f1_score.py
+++ f1_score.py
@@ -2,10 +2,9 @@ from datasets import load_dataset
 from transformers import BertTokenizer
 from torch.utils.data import Dataset, DataLoader
 from tqdm.auto import tqdm
+import torch
+from transformers import BertForQuestionAnswering
 
-dataset = load_dataset(
-   'drop')
-tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 
 def normalize_answer(s):
   """Lower text and remove punctuation, articles and extra whitespace."""
@@ -28,52 +27,76 @@ def create_segment_ids(input_ids):
   segment_ids = [0] * question_segment + [1] * passage_segment
   return segment_ids
 
-validation_data = dataset['validation']
-val_generator = DataLoader(validation_data, batch_size=8, collate_fn=lambda x: x,
-                           num_workers=2)
-precisions = []
-recalls = []
 
-for index, data in enumerate(tqdm(val_generator)):
-    for q in data:
-        answ = q['answers_spans']
-        que = ''.join(q['question'])
-        passage = ''.join(q['passage'])
+def test_accuracy(model, data_generator):
+    precisions = []
+    recalls = []
+    # model.to(device)
 
-        if 'span' or 'date' in list(list(zip(*answer['types']))[0]):
-            input_ids = tokenizer.encode(que,
-                                    passage,
-                                    max_length=384,
-                                    truncation="only_second")
-            segment_ids = create_segment_ids(input_ids)
+    for index, data in enumerate(tqdm(data_generator)):
+        for q in data:
+            answ = q['answers_spans']
+            que = ''.join(q['question'])
+            passage = ''.join(q['passage'])
+    
+            if 'span' or 'date' in list(list(zip(*answer['types']))[0]):
+                input_ids = tokenizer.encode(que,
+                                        passage,
+                                        max_length=384,
+                                        truncation="only_second")
+                segment_ids = create_segment_ids(input_ids)
+    
+                outputs = model(torch.tensor([input_ids]),
+                         token_type_ids=torch.tensor([segment_ids]),
+                         return_dict=True)
+                predicted_start = torch.argmax(outputs.start_logits)
+                predicted_end = torch.argmax(outputs.end_logits)
+                # normalize predicted answer
+                tokens = tokenizer.convert_ids_to_tokens(input_ids)
+                normalized_prediction = normalize_answer(''.join(
+                                        tokens[predicted_start:predicted_end+1]))
+                # get ground truth tokens
+                gold_tokens = ''.join(answ['spans'][0])
 
-            outputs = model(torch.tensor([input_ids]),
-                     token_type_ids=torch.tensor([segment_ids]),
-                     return_dict=True)
-            predicted_start = torch.argmax(outputs.start_logits)
-            predicted_end = torch.argmax(outputs.end_logits)
-            # normalize predicted answer
-            tokens = tokenizer.convert_ids_to_tokens(input_ids)
-            normalized_prediction = normalize_answer(''.join(
-                                    tokens[predicted_start:predicted_end+1]))
-            # get ground truth tokens
-            gold_tokens = ''.join(answ['spans'][0])
+                # check for digits
+                if normalized_prediction.strip().isdigit():
+                    if normalized_prediction.strip() == gold_tokens.strip()
+                        precision = 1
+                        recall = 1
+                    else:
+                        precision = 0
+                        recall = 0
+                # general case 
+                else:
+                    common = Counter(normalized_prediction) & Counter(gold_tokens)
+                    num_same = sum(common.values())
+    
+                # calculate precision, recall, f1
+    
+                if num_same == 0 or len(normalized_prediction) == 0:
+                  precision = 0
+                  recall = 0
+                else:
+                  precision = 1.0 * num_same / len(normalized_prediction)
+                  recall = 1.0 * num_same / len(gold_tokens)
+    
+                precisions.append(precision)
+                recalls.append(recall)
 
-            # calculate precision, recall, f1
-            common = Counter(normalized_prediction) & Counter(gold_tokens)
-            num_same = sum(common.values())
+    avg_recall = sum(recalls) / len(recalls)
+    avg_precision = sum(precisions) / len(precisions)
+    f1 = (2 * avg_precision * avg_recall) / (avg_precision + avg_recall)
+    return f1
 
-            if num_same or len(normalized_prediction) == 0:
-              precision = 0
-              recall = 0
-            else:
-              precision = 1.0 * num_same / len(normalized_prediction)
-              recall = 1.0 * num_same / len(gold_tokens)
+if __name__=="__main__":
+    dataset = load_dataset('drop')
+    tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    # device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-            precisions.append(precision)
-            recalls.append(recall)
-
-avg_recall = sum(recalls) / len(recalls)
-avg_precision = sum(precisions) / len(precisions)
-f1 = (2 * avg_precision * avg_recall) / (avg_precision + avg_recall)
-print(f1)
+    model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    validation_data = dataset['validation']
+    val_generator = DataLoader(validation_data,
+                                batch_size=8,
+                                collate_fn=lambda x: x,
+                                num_workers=2)
+    print(f'F1 Score: {test_accuracy(model, val_generator)}')
